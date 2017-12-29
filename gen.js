2a73992e3fbab0c09a2a79808e79874981883b6b
trafort <34495933+trafort@users.noreply.github.com>
2017-12-28 21:54:19 -0500

    Create gen.js

---
+++ gen.js
@@ -0,0 +1,6 @@
+gen()
+fuction gen()
+
+var buy = ["chromeos","Ipad","a House (1M)","A website","A domain","windows"]
+document.getElementById("get").innerHTML = ideas[Math.floor(Math.random() * ideas.length) + 0.1  ]
+	}
