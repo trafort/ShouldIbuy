4309978ccfd0865f2b38bf4e553436f2b78fa89d
trafort <34495933+trafort@users.noreply.github.com>
2017-12-28 21:58:07 -0500

    Delete gen.js

--- gen.js
+++
@@ -1,6 +0,0 @@
-gen()
-fuction gen()
-
-var buy = ["chromeos","Ipad","a House (1M)","A website","A domain","windows"]
-document.getElementById("get").innerHTML = ideas[Math.floor(Math.random() * ideas.length) + 0.1  ]
-	}
